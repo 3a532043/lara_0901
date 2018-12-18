6a1c284160e202a0d0a1c5c8c4e99de7cdc9d56e
3a532043 <s3a532043@student.ncut.edu.tw>
2018-12-18 20:58:37 +0800

    Validation_練習4_2

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -17,37 +17,23 @@
     </div>
 </div>
 <!-- /.row -->
-@if($errors->any())
-<div class="row">
-    <div class="col-lg-12">
-        <div class="alert alert-danger alert-dismissable">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            <i class="fa fa-info-circle"></i>  <strong>警告！</strong> 請修正表單錯誤：
-			            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </div>
-    </div>
-</div>
-@endif
+@include('admin.layouts.partials.validation')
 <!-- /.row -->
 
 <div class="row">
     <div class="col-lg-12">
-               <form action="/admin/posts/{{$post->id}}" method="POST" role="form">
+        <form action="/admin/posts/{{$post->id}}" method="POST" role="form">
             {{ csrf_field() }}
             {{ method_field('PATCH') }}
 
             <div class="form-group">
                 <label>標題：</label>
-                                <input name="title" class="form-control" placeholder="請輸入文章標題" value="{{$post->title}}">
+                <input name="title" class="form-control" placeholder="請輸入文章標題" value="{{$post->title}}">
             </div>
-
             <div class="form-group">
                 <label>內容：</label>
-                                <textarea name="content" class="form-control" rows="10">{{$post->content}}</textarea>
+                <textarea name="content" class="form-control" rows="10">{{$post->content}}</textarea>
             </div>
-
             <div class="form-group">
                 <label>精選？</label>
                 <select name="is_feature" class="form-control" >
@@ -55,12 +41,11 @@
                     <option value="1" {{ $post->is_feature?'SELECTED':'' }}>是</option>
                 </select>
             </div>
-
             <div class="text-right">
                 <button type="submit" class="btn btn-success">更新</button>
             </div>
-
         </form>
+
 
         <p>&nbsp;</p>
         <p>&nbsp;</p>
